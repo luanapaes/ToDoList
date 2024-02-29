b723b9dafcfb2f06134f6ffe8c141c5868de29fa
Luana Paes <luanafranca598@gmail.com>
2024-02-29 18:43:28 -0300

    adicionar todo e done todo
    até esta parte já é possível adicionar todos e marcá-los como concluídos(done).

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToDo } from './ToDo';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo-list';
+  todos: ToDo[] = [];
+  newTodo: string;
+
+  saveTodo(){
+    if(this.newTodo){
+      let todo = new ToDo();
+      todo.name = this.newTodo;
+      todo.isCompleted = true;
+      this.todos.push(todo);
+      this.newTodo = '';
+    }
+  }
+
+  done(id:number){
+    this.todos[id].isCompleted = !this.todos[id].isCompleted;
+  }
+
+  removeTodo(): void{
+    this.todos.pop();
+  }
 }
